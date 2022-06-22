72b1fa8ec94d2bbbea1d25e6903db281d6c54f78
Luis Monte <luismonte.l560@gmail.com>
2022-06-22 02:51:44 -0300

    feat(products/route): add create products route

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import productsController from '../controllers/products.controller';
+import { nameValidation, amountValidation } from '../middlewares/productMiddlewares';
 
 const router = Router();
 
 router.get('/', productsController.getAll);
+router.post('/', nameValidation, amountValidation, productsController.create);
 
 export default router;
