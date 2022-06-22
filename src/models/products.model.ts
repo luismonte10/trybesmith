f6f840a7565541d7ada2f7339fb94d2922036c41
Luis Monte <luismonte.l560@gmail.com>
2022-06-22 02:59:07 -0300

    feat(products/model): implements create/model model

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,7 +1,8 @@
+import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
-import Products from '../interfaces/products.interface';
+import { CreateProduct, Products } from '../interfaces/products.interface';
 
-const getAll = async () => {
+const getAll = async (): Promise<Products[]> => {
   const query = 'SELECT * FROM Trybesmith.Products';
 
   const [products] = await connection.execute(query);
@@ -9,6 +10,19 @@ const getAll = async () => {
   return products as Products[];
 };
 
+const create = async (newProduct: CreateProduct): Promise<CreateProduct> => {
+  const query = 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)';
+
+  const { name, amount } = newProduct;
+
+  const [productId] = await connection.execute<ResultSetHeader>(query, [name, amount]);
+
+  const { insertId } = productId;
+
+  return { id: insertId, ...newProduct };
+};
+
 export = {
   getAll,
+  create,
 };
